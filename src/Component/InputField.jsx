386d6f1d7182c3f4054364d335ac6cf30fa7485c
YarickVodila <y.koltashev@mail.ru>
2022-10-15 22:48:54 +0500

    cum it

--- src/Component/InputField.jsx
+++ src/Component/InputField.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Label from "./Label";
+import classes from './Styles/Containers.module.css'
 
 const InputField = ({children,...props}) => {
     return (
         <Label>
             {children}
-            <input {...props}/>
+            <input className={classes.input} placeholder=" введите размер в мм" {...props} />
         </Label>
     );
 };
