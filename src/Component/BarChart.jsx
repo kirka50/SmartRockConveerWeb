6a0020516235a722199642ae5480d0ff1a91952a
gabbhack <copyleft@sosus.org>
2022-10-16 13:24:02 +0500

    fix colors

--- src/Component/BarChart.jsx
+++ src/Component/BarChart.jsx
@@ -12,11 +12,11 @@ const BarChart = ({graph2Data}) => {
                          data: graph2Data.y,
                          backgroundColor: [
                              'rgba(5, 0, 255, 1)',
-                             'rgba(0, 209, 255, 1)',
                              'rgba(219, 0, 255, 1)',
-                             'rgba(255, 0, 0, 1)',
-                             'rgba(250, 255, 0, 1)',
+                             'rgba(0, 209, 255, 1)',
                              'rgba(0, 255, 56, 1)',
+                             'rgba(250, 255, 0, 1)',
+                             'rgba(225, 178, 178, 1)',
                              'rgba(225, 255, 255, 1)'
                          ],
                          animation: false
